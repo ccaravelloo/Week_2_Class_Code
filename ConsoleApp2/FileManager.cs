3554f21b55707366ed4d9c558fe45f7f279a662b
cscaravello <cscaravelloo@gmail.com>
2023-08-31 21:23:27 -0500

    Add project files.

---
+++ ConsoleApp2/FileManager.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp2
+{
+    public class FileManager
+    {
+        public void Write(string filename) 
+        {
+            StreamWriter sw = new StreamWriter(filename, true);
+            Console.WriteLine("Enter a course");
+            var course = Console.ReadLine();
+
+            Console.WriteLine("Enter a grade");
+            var grade = Console.ReadLine();
+
+
+            sw.WriteLine($"{course}|{grade}");
+
+            sw.Close();
+        }
+        public void Read(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                StreamReader sr = new StreamReader(filename);
+
+                var line = sr.ReadLine();
+                while (sr.EndOfStream != true)
+                {
+                    line = sr.ReadLine();
+
+                    //Console.WriteLine($"your line was {line}");
+
+                    var lineArray = line.Split('|');
+
+                    Console.WriteLine($"Course: {lineArray[0]}, Grade: {lineArray[1]}");
+                }
+
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FileManager fileManager = new FileManager();
+            
+            string filename = "grades.txt";
+            string menu;
+            do
+            {
+                Console.WriteLine("1. Edit File");
+                Console.WriteLine("2. Read File");
+                Console.WriteLine("3. Exit System");
+                menu = Console.ReadLine();
+
+                if (menu == "1")
+                {
+                    fileManager.Write(filename);
+                   
+                }
+                else if (menu == "2")
+                {
+                    fileManager.Read(filename);
+
+                }
+                else if (menu == "3")
+                {
+
+                }
+
+
+            } while (menu != "3");
+            
+        }
+    }
+}
+
+
+
+
+
+
+
